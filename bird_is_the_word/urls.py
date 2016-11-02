7c96f6904b9c2ff49dda4edb9c509bebc87115a8
MBAustin <austin.matthew.b@gmail.com>
2016-11-01 17:35:04 -0700

    Fix main index

--- bird_is_the_word/urls.py
+++ bird_is_the_word/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+import bird_quiz
 
 urlpatterns = [
+    url(r'^$', include('bird_quiz.urls')),
     url(r'^quiz/', include('bird_quiz.urls')),
     url(r'^admin/', admin.site.urls),
 ]
