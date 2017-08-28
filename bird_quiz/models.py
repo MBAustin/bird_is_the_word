e73d7b06d895d57527521d4a49389459ceebc966
MBAustin <austin.matthew.b@gmail.com>
2017-08-27 17:06:02 -0700

    Add changes to models

--- bird_quiz/models.py
+++ bird_quiz/models.py
@@ -1,10 +1,14 @@
 from django.db import models
 from django.core.validators import validate_comma_separated_integer_list
 
+
+class Group(models.Model):
+    name = models.CharField(max_length=140)
+
 class Bird(models.Model):
     common_name = models.CharField(max_length=140)
     latin_name = models.CharField(max_length=140, null=True, blank=True)
-    group = models.CharField(max_length=140)
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)
     bad_pics = models.CharField(max_length = 500, null=True, blank=True, validators=[validate_comma_separated_integer_list])
 
     def __str__(self):
